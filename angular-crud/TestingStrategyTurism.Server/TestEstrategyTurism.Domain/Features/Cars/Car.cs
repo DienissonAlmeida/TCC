e321f7c7bd8938df00a198524f5e042b23d52bad
Dienisson Chinelatto <dienissonchinelatto@gmail.com>
2019-03-24 21:56:55 -0300

    Adicionado classse de carros.

---
+++ angular-crud/TestingStrategyTurism.Server/TestEstrategyTurism.Domain/Features/Cars/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestEstrategyTurism.Domain.Features.Cars
+
+{
+    public class Car : Entity
+    {
+        public string Model { get; set; }
+
+        public string Brand { get; set; }
+
+        public int Passengers { get; set; }
+
+        public string Transmission { get; set; }
+
+        public bool AirConditioning { get; set; }
+
+        public string[] Photos { get; set; }
+    }
+}
---
+++ angular-crud/TestingStrategyTurism.Server/TestEstrategyTurism.Domain/Features/Hotels/Hotel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestEstrategyTurism.Domain.Features.Hotels
+{
+    public class Hotel : Entity
+    {
+        public string City { get; set; }
+
+        public string Name { get; set; }
+
+        public int Stars { get; set; }
+    }
+}
